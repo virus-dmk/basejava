d33c7ed5da4edb3d266bd4dfc57141f629dcbc79
Dima <virus_dmk@mail.ru>
2018-09-07 23:52:33 +0300

    lesson 2.1 change methods, deleted free spaces, code dublicates etc.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -14,71 +14,58 @@ public class ArrayStorage {
     }
 
     void update(Resume resume) {
-        if (resumeExists(resume)) {
-
+        Integer index = resumeExists(resume.uuid);
+        if (index != null) {
             System.out.printf("Resume updated!\n\n");
+        } else {
+            System.out.printf("Resume not exists\n\n");
         }
     }
 
-    void save(Resume r) {
-        if (resumeExists(r)) {
-            return;
+    void save(Resume resume) {
+        if (resumeExists(resume.uuid) != null) {
+            System.out.printf("Resume already exists \n\n");
         } else {
             if (size == storage.length) {
-                System.out.println("Storage is full/n/n");
+                System.out.println("Storage is full\n\n");
             } else {
-
-                storage[size++] = r;
-                System.out.printf("Saved Resume, uuid: %s \n/n", r.uuid);
+                storage[size++] = resume;
+                System.out.printf("Resume saved, uuid: %s \n", resume.uuid);
             }
         }
-
     }
 
     Resume get(String uuid) {
-        if (resumeExists(uuid)) {
-            for (int i = 0; i < size; i++) {
-                if (storage[i].uuid.equals(uuid)) {
-                    return storage[i];
-                }
-            }
+        Integer index = resumeExists(uuid);
+        if (index != null) {
+            return storage[index];
+        } else {
+            System.out.printf("Resume not exists\n\n");
+            return null;
         }
-        return null;
     }
 
     void delete(String uuid) {
-        if (resumeExists(uuid)) {
-            for (int i = 0; i < size; i++) {
-                if (storage[i].uuid.equals(uuid)) {
-                    System.arraycopy(storage, i + 1, storage, i, size - 1 - i);
-                    storage[size - 1] = null;
-                    size--;
-                    break;
-                }
-            }
+        Integer index = resumeExists(uuid);
+        if (index != null) {
+            storage[index] = storage[size - 1];
+            storage[size - 1] = null;
+            size--;
         }
     }
 
-    boolean resumeExists(String uuid) {
+    /**
+     * @param uuid unuque id
+     * @return if exist, return index of object in Resume massive,
+     * else return null
+     */
+    Integer resumeExists(String uuid) {
         for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
-                System.out.printf("Resume exists! \n");
-                return true;
+                return i;
             }
         }
-        System.out.printf("Resume not exists! \n");
-        return false;
-    }
-
-    boolean resumeExists(Resume r) {
-        for (int i = 0; i < size; i++) {
-            if (r.uuid.equals(storage[i].uuid)) {
-                System.out.printf("Resume exists! \n");
-                return true;
-            }
-        }
-        System.out.printf("Resume not exists! \n");
-        return false;
+        return null;
     }
 
     /**
